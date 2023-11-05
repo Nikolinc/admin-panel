6fa4c27776736c807f4d1015e937dc9477b9b1b4
Nikolinc <tsigulskynikita@gmail.com>
2023-11-05 22:43:56 +0500

    made the navbar the root element

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import AdminPanel from "../pages/adminPanel/adminPanel";
 import ErrorPage from "../pages/error/error";
+import NavBar from "components/navbar/navbar";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AdminPanel />,
+    element: <NavBar />,
     errorElement: <ErrorPage />,
+    children: [{
+      index: true,
+      path: "/admin-panel",
+      element: <AdminPanel />,
+    }]
   },
 
 ]);
