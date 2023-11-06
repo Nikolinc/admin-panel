c3efd0d41a86cb9669b46eb44adae9fecf764ea7
Nikolinc <tsigulskynikita@gmail.com>
2023-11-06 15:30:16 +0500

    rm path admin panel

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import AdminPanel from "../pages/adminPanel/adminPanel";
-import ErrorPage from "../pages/error/error";
+import AdminPanel from "pages/adminPanel/adminPanel";
+import ErrorPage from "pages/error/error";
 import NavBar from "components/navbar/navbar";
 
 const router = createBrowserRouter([
@@ -8,11 +8,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <NavBar />,
     errorElement: <ErrorPage />,
-    children: [{
-      index: true,
-      path: "/admin-panel",
-      element: <AdminPanel />,
-    }]
+    children: [
+      {
+        index: true,
+        element: <AdminPanel />,
+      }
+    ]
   },
 
 ]);
