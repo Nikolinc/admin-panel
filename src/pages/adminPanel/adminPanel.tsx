86fb450c919366994d1fd0058d3ecd10f21ed661
Nikolinc <tsigulskynikita@gmail.com>
2023-11-06 15:31:03 +0500

    add header and user list

--- src/pages/adminPanel/adminPanel.tsx
+++ src/pages/adminPanel/adminPanel.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './adminpanel.css'
+import { ReactComponent as Search } from 'assets/icons/search.svg';
+import UserList from 'components/userList/userList';
 
 function AdminPanel() {
+
+
   return (
-    <div>adminPanel</div>
+    <div className='admin-panel'>
+      <div className="header">
+        <h3> Команда</h3>
+        <label className='search'>
+          <input type="text" placeholder='Поиск по Email'/>
+
+
+          <div className='search-checkbox'>
+            <input type="checkbox" id="search-icon" />
+            <label htmlFor="search-icon"> <Search /></label>
+          </div>
+        </label>
+        <button className='add-user'>Добавить пользователя</button>
+      </div>
+      <UserList />
+    </div>
   )
 }
 
